82f2f9af0685a3488c6467255d945d00a543f2a5
TheMaximeSan <mxm.tournier@gmail.com>
2021-07-26 22:25:04 +0200

    Cin

--- main.cpp
+++ main.cpp
@@ -15,6 +15,27 @@ int main() {
     cout << "qu'elle est ton age ?" << endl;
     cout << "Mon age est de : " << agePersonne << endl;
 
+    int qiUtilisateur(150);
+    string nomUtilisateur("Albert Einstein");
+    cout << "Vous vous appelez " << nomUtilisateur << " et votre QUi vaut " << qiUtilisateur << endl;
+
+    int& maRef(agePersonne);
+    cout << "Variable origine : " << agePersonne << " = et la variable via ref " << maRef << endl;
+
+    cout << "C'est quoi ton prenom ?" << endl;
+    string resultName("null");
+    cin >> resultName;
+
+    cout << "Qu'elle est ton age :" << endl;
+    int resultAge(0);
+    cin >> resultAge;
+
+    cout << "Combien fait PI pour toi ?" << endl;
+    double resultPi(-1);
+    cin >> resultPi;
+
+    cout << "Vous vous appelez " << resultName << " et vous avez " << resultAge << " ans et vous pensez que pi est égal a " << resultPi << endl;
+
 
     return 0;
 }
