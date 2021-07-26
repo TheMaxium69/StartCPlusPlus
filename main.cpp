51f5c736c58eacc20a33bc53750f56295fbdd989
TheMaximeSan <mxm.tournier@gmail.com>
2021-07-26 21:58:08 +0200

    Manage de mes première variable

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello Beaux Gosse" << endl << "Comment tu vas ?" << endl;
+    cout << "Moi je vais bien" << endl;
+    cout << "je suis la première application de maxime en c++ : " ;
+    cout << "\\ test" << endl;
+
+    string myMessage("je suis une variable");
+    cout << myMessage << endl;
+
+    int agePersonne(17);
+    cout << "qu'elle est ton age ?" << endl;
+    cout << "Mon age est de : " << agePersonne << endl;
+
+
     return 0;
 }
